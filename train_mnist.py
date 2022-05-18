b317e8a0ab22657ed520292960c11974588cbbdb
kaluskam <01151384@pw.edu.pl>
2022-05-18 23:02:25 +0200

    c

--- train_mnist.py
+++ train_mnist.py
@@ -15,24 +15,24 @@ images, labels = mndata.load_testing()
 images = np.array(images)
 labels = np.array(labels)
 
-KN = Network(input_shape=len(images[0]), shape=(2, 5), grid_type='rect', name='KN_mnist_100_rect')
-KN.load_weights()
-#KN.name = 'KN_mnist_100_rect'
-KN.fit(x_train=images, y_train=labels, start_epoch=100, n_epochs=200, neighbourhood_scale=1)
+# KN = Network(input_shape=len(images[0]), shape=(2, 5), grid_type='rect', name='mnist_500_rect')
+
+# #KN.name = 'KN_mnist_100_rect'
+# KN.fit(x_train=images, y_train=labels, start_epoch=0, n_epochs=100, neighbourhood_scale=1)
+# print(KN.evaluate())
+# KN.plot()
+#
+KN = Network(input_shape=len(images[0]), shape=(2, 5), grid_type='rect', name='mnist_100_rect_0_6')
+KN.fit(x_train=images, y_train=labels, n_epochs=100, neighbourhood_scale=0.6)
 print(KN.evaluate())
 KN.plot()
 
-KN = Network(input_shape=len(images[0]), shape=(2, 5), grid_type='rect', name='KN_mnist_100_rect_0_6')
-KN.fit(x_train=images, y_train=labels, n_epochs=200, neighbourhood_scale=0.6)
-print(KN.evaluate())
-KN.plot()
-
-KN = Network(input_shape=len(images[0]), shape=(2, 5), grid_type='hex', name='KN_mnist_100_hex')
+KN = Network(input_shape=len(images[0]), shape=(2, 5), grid_type='hex', name='mnist_100_hex')
 KN.fit(x_train=images, y_train=labels, n_epochs=100, neighbourhood_scale=1)
 print(KN.evaluate())
 KN.plot()
 
-KN = Network(input_shape=len(images[0]), shape=(2, 5), grid_type='hex', name='KN_mnist_100_hex_0_6')
+KN = Network(input_shape=len(images[0]), shape=(2, 5), grid_type='hex', name='mnist_100_hex_0_6')
 KN.fit(x_train=images, y_train=labels, n_epochs=100, neighbourhood_scale=0.6)
 print(KN.evaluate())
 KN.plot()
